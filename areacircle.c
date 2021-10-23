0a26de504e2642373e1586f174d144784837aef5
Tenzing Rabgay Bhutia <rabgay497@gmail.com>
2021-10-23 12:21:06 +0530

    initial commit

---
+++ areacircle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+	float area,r;
+	printf("Enter the radius of aa circle\n");
+	scanf("%f",&r);
+	area=3.14*r*r;
+	printf("The area of the circle is %.2f",area);
+	return 1;
+	
+	
+}
+
